c1b591ca3e525151871270dbfce6a9759544c30e
Marty445 <markovskimarty@gmail.com>
2025-12-18 13:04:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ st.title("Моето първо Streamlit приложение")
 name = st.text_input("Как се казваш?")
 if name:
    st.write(f"Здравей, {name}!")
+age = st.number_input(
+    "Enter your age",
+    min_value=0,
+    max_value=120,
+    value=25,
+    step=1
+)
+st.write(f"Your age is {age}")
